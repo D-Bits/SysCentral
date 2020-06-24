a271f830fe29dfdb9428f2eb5e11ea81b4012c2b
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-06-24 02:09:32 -0700

    Added exception handling.

--- Program.cs
+++ Program.cs
@@ -21,39 +21,42 @@ namespace SysCentral
         }
 
         public static void Main(string[] args)
-        {        
-            UserOptions();    
-            Console.Write("Enter an int, based on the above options: ");
-            int userChoice = Convert.ToInt32(Console.ReadLine());
+        {
+            try:        
+                UserOptions();    
+                Console.Write("Enter an int, based on the above options: ");
+                int userChoice = Convert.ToInt32(Console.ReadLine());
 
-            if (userChoice == 0)
-            {
+                if (userChoice == 0)
+                {
 
-            }
-            else if (userChoice == 1)
+                }
+                else if (userChoice == 1)
+                {
+                    FileSys.FileSysOptions();
+                    Console.WriteLine();
+                }
+                else if (userChoice == 2)
+                {
+                    Updates.UpdatesOptions();
+                    Console.WriteLine();
+                }
+                else if (userChoice == 3)
+                {
+                    SysInfo.SysInfoOptions();
+                    Console.WriteLine();
+                }
+                else 
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid option. Please enter a valid int.");
+                    Console.WriteLine();
+                    Main(null);
+                }
+            catch (Exception ex)
             {
-                FileSys.FileSysOptions();
-                Console.WriteLine();
-            }
-            else if (userChoice == 2)
-            {
-                Updates.UpdatesOptions();
-                Console.WriteLine();
-            }
-            else if (userChoice == 3)
-            {
-                SysInfo.SysInfoOptions();
-                Console.WriteLine();
-            }
-            else 
-            {
-                Console.WriteLine();
-                Console.WriteLine("Invalid option. Please enter a valid int.");
-                Console.WriteLine();
-                Main(null);
-            }
-
-            
+                Console.WriteLine(ex.Message);
+            }   
         }
     }
 }
