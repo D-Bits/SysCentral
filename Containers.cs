3f2cb44361caa36236b672f67aa878a1bd805b56
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2021-06-26 12:02:55 -0700

    Added functions

--- Containers.cs
+++ Containers.cs
@@ -6,22 +6,43 @@ namespace SysCentral
 {
     public class Containers
     {
+        // Boot a container, plus assign it a name and (optionally) a volume.
         public Process BootContainer(string image, string name, string volume)
         {
             if (volume == "")
+            {
+                Process createContainer = Process.Start("powershell.exe", $"docker run {image} --name {name}");
+
+                return createContainer;
+            }
+            else
             {
                 Process createContainer = Process.Start("powershell.exe", $"docker run {image} --name {name} -v {volume}");
 
                 return createContainer;
             }
-            
         }
 
+        // Stop a running, named container
         public Process StopContainer(string name)
         {
             Process stopContainer = Process.Start("powershell.exe", $"docker stop {name}");
 
             return stopContainer;
+        }
+
+        // Restart a named container
+        public Process RestartContainer(string name)
+        {
+            Process restartContainer = Process.Start("powershell.exe", $"docker restart {name}");
+
+            return restartContainer;
+        }
+
+        // Run docker system prune
+        public Process PruneSystem()
+        {
+            Process prune = Process.Start("powershell.exe", "docker system prune");
         }
     }
 }
