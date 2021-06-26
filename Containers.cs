550eb2c9fe16ab5cd3b6dd5e10525ea26d5b9ec8
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2021-06-26 12:51:27 -0700

    Added Postgres container function

--- Containers.cs
+++ Containers.cs
@@ -6,21 +6,20 @@ namespace SysCentral
 {
     public class Containers
     {
-        // Boot a container, plus assign it a name and (optionally) a volume.
-        public Process BootContainer(string image, string name, string volume)
+        // Boot a generic container, plus assign it a name and (optionally) a volume.
+        public Process BootContainer(string image, string name)
         {
-            if (volume == "")
-            {
-                Process createContainer = Process.Start("powershell.exe", $"docker run {image} --name {name}");
+            Process createContainer = Process.Start("powershell.exe", $"docker run {image} --name {name}");
 
-                return createContainer;
-            }
-            else
-            {
-                Process createContainer = Process.Start("powershell.exe", $"docker run {image} --name {name} -v {volume}");
+            return createContainer;
+        }
 
-                return createContainer;
-            }
+        // Boot a named Postgres container, with a volume, and specified version
+        public Process BootPostgres(string name, string volume, double version)
+        {
+            Process createContainer = Process.Start("powershell.exe", $"docker run postgres:{version}-alpine --name {name}");
+
+            return createContainer;
         }
 
         // Stop a running, named container
@@ -43,6 +42,60 @@ namespace SysCentral
         public Process PruneSystem()
         {
             Process prune = Process.Start("powershell.exe", "docker system prune");
+
+            return prune;
+        }
+
+        // Run docker system info
+        public Process SystemInfo()
+        {
+            Process sysinfo = Process.Start("powershell.exe", "docker system info");
+
+            return sysinfo;
+        }
+
+        public static void ContainerChoices()
+        {
+            Dictionary<int, string> ContainerOptions = new Dictionary<int, string>();
+
+            ContainerOptions.Add(0, "Back to Main Menu.");
+            ContainerOptions.Add(1, "Boot a Container.");
+            ContainerOptions.Add(2, "Stop a Running Container.");
+            ContainerOptions.Add(3, "Restart a Running Container.");
+            ContainerOptions.Add(4, "Run docker system prune.");
+            ContainerOptions.Add(5, "Get Docker System Info");
+
+            Console.WriteLine();
+
+            foreach (var option in ContainerOptions)
+            {
+                Console.WriteLine(option);
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Select an option, based on the above options: ");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (userChoice == 0)
+            {
+                Program.Main(null);
+            }
+            else if (userChoice == 1)
+            {
+                // Get image name from stdin
+                Console.Write("Enter an image name: ");
+                string imageName = Console.ReadLine();
+
+                // Get container name from stdin
+                Console.Write("Enter a name for your container: ");
+                string containerName = Console.ReadLine();
+
+                // Get a volume name from stdin
+                Console.Write("Enter a name for your volume (leave blank to not use a volume: ");
+            }
         }
     }
 }
